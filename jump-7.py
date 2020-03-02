c41cc40fd665a0d4c9ad678cb6a79be12c1d87d2
chard-li72 <434668824@qq.com>
2020-03-02 20:07:45 +0800

    it is right

---
+++ jump-7.py
@@ -0,0 +1,9 @@
+for i in range (1, 70):
+    if i % 7 != 0 and (i-7) % 10 != 0 :
+        print(i)
+        i += 1
+for i in range (80, 101):
+    if i % 7 != 0 and (i-7) % 10 != 0 :
+        print(i)
+        i += 1
+
